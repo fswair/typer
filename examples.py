a7684aa56e4d699278722cbb7f46f338128dc064
Mert <62549656+fswair@users.noreply.github.com>
2025-07-28 20:16:10 +0300

    Update examples.py

--- examples.py
+++ examples.py
@@ -8,4 +8,4 @@ print(validated_code)
 #> typing.Union[typing.Callable[[], builtins.int], builtins.int]
 
 validate_types('FooBar', 'str') # NameError `FooBar`
-validate_types('int', 'str') # None, it means validated
+validate_types('int', 'str') # Always returns true
