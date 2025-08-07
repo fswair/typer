1b3fe41c4d804ad4068848c02ffe6a89ff28d9bc
Mert <62549656+fswair@users.noreply.github.com>
2025-08-07 23:31:47 +0300

    v2

--- examples.py
+++ examples.py
@@ -1,11 +1,19 @@
-from validator import validate_names
+from validator import TypeValidator
 
 code = 'Union[Callable[[], int], int]'
 
-validated_code = validate_names(code=code)
+validator = TypeValidator(code)
 
-print(validated_code)
+validation = validator.validate_names()
+
+print(validation)
+
+#> validated_type='typing.Union[typing.Callable[[], builtins.int], builtins.int]'
+# errors=[] pytype=typing.Union[typing.Callable[[], int], int] invalid_names=[]
+# type_map={'int': 'builtins', 'Callable': 'typing', 'Union': 'typing'}
+
+print(validation.pytype)
+#> typing.Union[typing.Callable[[], int], int]
+
+print(validation.validated_type)
 #> typing.Union[typing.Callable[[], builtins.int], builtins.int]
-
-validate_types('FooBar', 'str') # NameError `FooBar`
-validate_types('int', 'str') # Always returns true
